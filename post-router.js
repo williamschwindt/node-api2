988a3ed0ef0d7aa4ac6dcc0f472759c9ca88e58c
William Schwindt <williammschwindt@gmail.com>
2020-04-13 12:56:08 -0700

    posts working

--- post-router.js
+++ post-router.js
@@ -21,4 +21,29 @@ router.post('/', (req, res) => {
         })
 })
 
+router.post('/:id/comments', (req, res) => {
+    let post = db.findById(req.params.id);
+    if(!post) {
+        return res.status(404).json({
+            message: "The post with the specified ID does not exist"
+        })
+    }
+
+    if(!req.body.text) {
+        return res.status(400).json({
+            errorMessage: "Please provide text for the comment"
+        })
+    }
+
+    db.insertComment(req.body)
+        .then(comment => {
+            return res.status(201).json(comment)
+        })
+        .catch(err => {
+            return res.status(500).json({
+                error: "There was an error while saving the comment to the database"
+            })
+        })
+})
+
 module.exports = router;
